6ede5a9e0aba03f2d4b83d0318b58b0bb2a95458
sugawara yuto <117239251+nattoSG09@users.noreply.github.com>
2023-12-12 07:08:20 +0900

    Quaternionどうしようかな

--- Engine/Components/Transform.cpp
+++ Engine/Components/Transform.cpp
@@ -10,11 +10,21 @@ Transform::Transform()
 	matTranslate_ = XMMatrixIdentity();
 	matRotate_ = XMMatrixIdentity();
 	matScale_ = XMMatrixIdentity();
+
+	rotationAxis_ = XMVectorSet(0, 0, 0, 0);
 }
 
 void Transform::Calclation()
 {
+	// position_.xyzの値をもとに_平行移動行列(移動量は０)_を作成
+	matTranslate_ = XMMatrixTranslation(position_.x, position_.y, position_.z);
 
+	// rotate_.xyzの値をもとに_回転行列_を作成
+	
+
+
+	// scale_.xyzの値をもとに_拡大縮小行列_を作成
+	matScale_ = XMMatrixScaling(scale_.x, scale_.y, scale_.z);
 }
 
 XMMATRIX Transform::WoaldMatrix()
--- Engine/Components/Transform.h
+++ Engine/Components/Transform.h
@@ -6,14 +6,18 @@ using namespace DirectX;
 
 class Transform : public Component
 {
+public:
+	XMFLOAT3 position_;		/*位置*/
+	XMFLOAT3 rotate_;		/*方向*/
+	XMFLOAT3 scale_;		/*拡大率*/
+
 private:
 	XMMATRIX matTranslate_;	/*移動行列*/
 	XMMATRIX matRotate_;	/*回転行列*/
 	XMMATRIX matScale_;		/*拡大行列*/
 
-	XMFLOAT3 position_;		/*位置*/
-	XMFLOAT3 rotate_;		/*方向*/
-	XMFLOAT3 scale_;		/*拡大率*/
+
+	XMVECTOR rotationAxis_;
 
 	Transform* pPalent_;	/*親オブジェクトの変換行列*/
 public:
@@ -28,6 +32,10 @@ public:
 	XMFLOAT3 Rotate() { return rotate_; }
 	void SetRotate(const XMFLOAT3 _rotate) { rotate_ = _rotate; }
 	void SetRotate(float _x, float _y, float _z) { SetRotate(XMFLOAT3(_x, _y, _z)); }
+
+	void UseQuaternion(XMVECTOR _rotateAxis);
+
+
 
 	XMFLOAT3 Scale() { return scale_; }
 	void SetScale(const XMFLOAT3 _scale) { scale_ = _scale; }
