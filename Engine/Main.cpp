69866f45fcf5c6c82925a1d4573ce3e327db131e
悠人 菅原 <220369@jc-21.jp>
2023-12-18 12:53:20 +0900

    ふぁ

--- Engine/Main.cpp
+++ Engine/Main.cpp
@@ -2,7 +2,6 @@
 
 int APIENTRY WinMain(HINSTANCE _hInstance,HINSTANCE _hPrevInst,LPSTR _lpCmdLine,int _nCmdShow)
 {
-
 	Application* application = new Application;
 	if (application->Initialize(_hInstance, _nCmdShow))
 	{
