91f6b592c4b394ba09d84fa7211ee252378318c8
悠人 菅原 <220369@jc-21.jp>
2023-12-19 17:33:35 +0900

    fa

---
+++ Texture.cpp
@@ -0,0 +1,45 @@
+#include "Texture.h"
+
+#include <filesystem>
+#include <locale>
+#include <codecvt>
+namespace fs = std::filesystem;
+
+namespace {
+	std::wstring StringToWString(const std::string& str) {
+		std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+		return converter.from_bytes(str);
+	}
+}
+
+Texture::Texture()
+{
+}
+
+Texture::~Texture()
+{
+}
+
+bool Texture::Load(string _filePath)
+{
+	SetFilePath(_filePath);
+	TexMetadata metaData;
+	ScratchImage scImage;
+
+	//画像ファイルを読み込む
+	if (LoadImageFile(_filePath, metaData, scImage) == false)return false;
+
+	return true;
+}
+
+bool Texture::LoadImageFile(string _filePath, TexMetadata& _metaData, ScratchImage& _scImage)
+{
+	//拡張子を取得する
+	fs::path filePath = _filePath;
+	std::string ext = filePath.extension().string();
+
+	/*if (ext == "png" || ext == "jpg") {
+		if(FAILED(LoadFromWICFile(StringToWString(_filePath).c_str(),WIC_FLAGGS_NONE,_metaData,_scImage)))
+	}*/
+	return false;
+}
---
+++ Texture.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include <DirectXTex.h>
+
+#pragma comment(lib,"DirectXTex.lib")
+
+using std::string;
+using namespace DirectX;
+
+class Texture
+{
+private:
+	string filePath_;
+public:
+	Texture();
+	~Texture();
+
+	bool Load(string _filePath);
+	void SetFilePath(string _filePath) { filePath_ = _filePath; }
+	bool LoadImageFile(string _filePath, TexMetadata& _metaData, ScratchImage& _scImage);
+};
+
